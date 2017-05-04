824ead240af4cd691d98c5fb453cd2f2e329e231
sunzhongyuan <sun_zhongyuan@sina.com>
2017-05-04 15:14:13 +0800

    models

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from . import models
 #from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
     #return HttpResponse('Hello world')
-    return render(request,'blog/index.html',{'hello':'HELLO BLOG'})
+    #return render(request,'blog/index.html',{'hello':'HELLO BLOG'})
+    article = models.Article.objects.get(pk=1)
+    return render(request,'blog/index.html',{'article':article})
