5d9964338cfb2ebf5ee2201518845cd7aeb3c948
sunzhongyuan <sun_zhongyuan@sina.com>
2017-05-10 10:42:51 +0800

    新增文章

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^admin/$', admin.site.urls),
     url(r'^index/$', views.index, name='index'),
     url(r'^index/(?P<article_id>[0-9]+)$', views.article_page, name='article_page'),
+    url(r'^edit$', views.edit_page, name='edit_page'),
+    url(r'^edit/action$', views.edit_action, name='edit_action'),
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -13,3 +13,15 @@ def index(request):
 def article_page(request,article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request,'blog/article_page.html',{'article':article})
+
+def edit_page(request):
+    return render(request,'blog/edit_page.html')
+
+def edit_action(request):
+    title = request.POST.get('title','title')
+    content = request.POST.get('content','content')
+    models.Article.objects.create(title=title,content=content)
+    return index(request)
+    
+    #articles = models.Article.objects.all()
+    #return render(request,'blog/index.html',{'articles':articles})
