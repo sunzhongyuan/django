c74dc3da188fb15617a1be54c3a62dc7604dff91
sunzhongyuan <sun_zhongyuan@sina.com>
2017-05-11 10:22:53 +0800

    修改删除文章

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^admin/$', admin.site.urls),
     url(r'^index/$', views.index, name='index'),
     url(r'^index/(?P<article_id>[0-9]+)$', views.article_page, name='article_page'),
-    url(r'^edit$', views.edit_page, name='edit_page'),
+    url(r'^edit/(?P<article_id>[0-9]+)$', views.edit_page, name='edit_page'),
+    url(r'^del/(?P<article_id>[0-9]+)$', views.article_del, name='article_del'),
     url(r'^edit/action$', views.edit_action, name='edit_action'),
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -14,14 +14,30 @@ def article_page(request,article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request,'blog/article_page.html',{'article':article})
 
-def edit_page(request):
-    return render(request,'blog/edit_page.html')
+def edit_page(request,article_id):
+    if str(article_id) == '0':
+        return render(request,'blog/edit_page.html')
+    else:
+        article = models.Article.objects.get(pk=article_id)
+        return render(request,'blog/edit_page.html',{'article':article})
 
 def edit_action(request):
+    article_id = request.POST.get('article_id','0')
     title = request.POST.get('title','title')
     content = request.POST.get('content','content')
-    models.Article.objects.create(title=title,content=content)
-    return index(request)
-    
+    if article_id == '0':
+        models.Article.objects.create(title=title,content=content)
+        return index(request)
+    else:
+        article = models.Article.objects.get(pk=article_id)
+        article.title = title
+        article.content = content
+        article.save()
+        return article_page(request,article_id)
+
     #articles = models.Article.objects.all()
     #return render(request,'blog/index.html',{'articles':articles})
+
+def article_del(request,article_id):
+    models.Article.objects.get(pk=article_id).delete()
+    return index(request)
